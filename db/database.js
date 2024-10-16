14bd34abcb44d5b9ee70a37d5db3d52210d7e54b
milsimural <108474929+milsimural@users.noreply.github.com>
2024-10-16 23:26:29 +0300

    env url setup

--- db/database.js
+++ db/database.js
@@ -1,10 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'mafiamanager',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
   },
   test: {
     username: 'root',
