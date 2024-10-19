37a51195350bc29605e118102a906b246b35bbb0
milsimural <108474929+milsimural@users.noreply.github.com>
2024-10-19 10:41:38 +0300

    Добавляю роутер

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import Layout from "./components/Layout";
+import NotesPage from "./components/pages/NotesPage";
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/notes",
+          element: <NotesPage />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-
+      <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/ui/NavigationComp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavigationComp() {
+  return (
+    <nav className="navigation">
+        <a href="">Home</a>
+        <a href="">Notes</a>
+    </nav>
+  )
+}
