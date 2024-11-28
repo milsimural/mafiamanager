34561389ea5e0ee9ddb488dca06bcfc8f3cb2d45
milsimural <108474929+milsimural@users.noreply.github.com>
2024-11-29 00:19:35 +0300

    готов загрузчик из рав даты результатов на сервак

--- server/src/routes/tournamentRouter.js
+++ server/src/routes/tournamentRouter.js
@@ -1,4 +1,4 @@
-const {Router} = require('express')
+const { Router } = require('express');
 
 const { Tournament, City } = require('../../db/models');
 
@@ -16,67 +16,75 @@ tournamentRouter.get('/', async (req, res) => {
 });
 
 tournamentRouter.get('/details/:id', async (req, res) => {
-    try {
-        const tournamentId = req.params.id;
-        const tournamentData = await Tournament.findByPk(tournamentId);
-        res.json(tournamentData);
-    } catch (error) {
-        res.status(500).json({error: `Ошибка при получении информации о турнире: ${error.message}`})
-    }
-})
+  try {
+    const tournamentId = req.params.id;
+    const tournamentData = await Tournament.findByPk(tournamentId);
+    res.json(tournamentData);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Ошибка при получении информации о турнире: ${error.message}` });
+  }
+});
 
 tournamentRouter.post('/add', async (req, res) => {
-    try {
-        const tournament = req.body;
-        const newTournament = await Tournament.create(tournament);
-        res.status(201).json(newTournament);
-    } catch (error) {
-        res.status(500).json({error: `Ошибка при добавлении турнира: ${error.message}`})
-    }
-})
+  try {
+    const tournament = req.body;
+    const newTournament = await Tournament.create(tournament);
+    res.status(201).json(newTournament);
+  } catch (error) {
+    res.status(500).json({ error: `Ошибка при добавлении турнира: ${error.message}` });
+  }
+});
 
 tournamentRouter.patch('/update/:id', async (req, res) => {
-    try {
-        const tournamentId = req.params.id;
-        const updatesData = req.body;
+  try {
+    const tournamentId = req.params.id;
+    const updatesData = req.body;
 
-        const [updatedRowsCount, updatedRows] = await Tournament.update(updatesData, {
-            where: { id: tournamentId},
-            returning: true,
-        })
+    const [updatedRowsCount, updatedRows] = await Tournament.update(updatesData, {
+      where: { id: tournamentId },
+      returning: true,
+    });
 
-        if (updatedRowsCount === 0) {
-            return res.status(404).json({ error: 'Турнир не найден' });
-          }
-        const updatedTournament = updatedRows[0];
-        res.json(updatedTournament);
-    } catch (error) {
-        res.status(500).json({ error: `Ошибка при обновлении турнира: ${error.message}` });
+    if (updatedRowsCount === 0) {
+      return res.status(404).json({ error: 'Турнир не найден' });
     }
-})
+    const updatedTournament = updatedRows[0];
+    const { resultTable, ...responseWithoutResultTable } = updatedTournament.get({
+      plain: true,
+    });
+
+    res.json(responseWithoutResultTable);
+  } catch (error) {
+    res.status(500).json({ error: `Ошибка при обновлении турнира: ${error.message}` });
+  }
+});
 
 tournamentRouter.delete('/delete/:id', async (req, res) => {
-    try {
-        const tournamentId = req.params.id;
-        const deletedRowsCount = await Tournament.destroy({
-            where: { id: tournamentId },
-        });
-        if (deletedRowsCount === 0) {
-            return res.status(404).json({ error: 'Турнир не найден' });
-          }
-        res.sendStatus(204);
-    } catch (error) {
-        res.status(500).json({ error: `Ошибка при удалении турнира: ${error.message}` });
+  try {
+    const tournamentId = req.params.id;
+    const deletedRowsCount = await Tournament.destroy({
+      where: { id: tournamentId },
+    });
+    if (deletedRowsCount === 0) {
+      return res.status(404).json({ error: 'Турнир не найден' });
     }
-})
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ error: `Ошибка при удалении турнира: ${error.message}` });
+  }
+});
 
 tournamentRouter.get('/cities', async (req, res) => {
-    try {
-        const cities = await City.findAll();
-        res.json(cities);
-    } catch (error) {    
-        res.status(500).json({ error: `Ошибка при получении списка городов: ${error.message}` });
-    }
-})
+  try {
+    const cities = await City.findAll();
+    res.json(cities);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Ошибка при получении списка городов: ${error.message}` });
+  }
+});
 
 module.exports = tournamentRouter;
