cd200469987d009404a7ca8ffdc1ba051f0f8667
milsimural <108474929+milsimural@users.noreply.github.com>
2024-12-03 22:55:19 +0300

    Готова верстка страницы турниров

--- client/src/components/pages/Tournaments/TournamentResult.jsx
+++ client/src/components/pages/Tournaments/TournamentResult.jsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import styles from './TournamentResult.module.css'
-import axiosInstance from 'src/axiosInstance'
+import React, { useEffect, useState } from "react";
+import styles from "./TournamentResult.module.css";
+import axiosInstance from "src/axiosInstance";
 
-export default function TournamentResult({user, tournament, roster}) {
-const [resultData, setResultData] = useState({})
+export default function TournamentResult({ user, tournament, roster }) {
+  const [resultData, setResultData] = useState({});
 
-    useEffect(() => {
-    if(!user || !tournament || !roster) return
+  useEffect(() => {
+    if (!user || !tournament || !roster) return;
 
     async function getResultData() {
-        try {
-            const response = await axiosInstance.get(`tournaments/details/${tournament.id}`);
-            setResultData(JSON.parse(response.data.resultTable));
-        } catch (error) {
-            console.error("Проблемы при загрузке данных турнира:", error);
-        };
+      try {
+        const response = await axiosInstance.get(
+          `constract/getrosters/${tournament.id}`
+        );
+        setResultData(response.data);
+      } catch (error) {
+        console.error("Проблемы при загрузке данных турнира:", error);
+      }
     }
     getResultData();
-}, [user, tournament, roster])
+  }, [user, tournament, roster]);
 
-// Выводем рейтинг и ростеры юзеров
+  // Выводем рейтинг и ростеры юзеров
   return (
     <>
-    <h1>Результаты</h1>
-<div className={StyleSheet.results}>
-    
-</div>
+      <h1>Результаты</h1>
+      <div className={StyleSheet.results}></div>
     </>
-  )
+  );
 }
+
+// заберем все ростеры
+// подгрузим всех плееров каждого ростера
+// сделаем табличку и вывдеме красиво результаты и морды плееров с капитаном
+// напишем какие призы выйграли плеере
--- client/src/components/pages/Tournaments/TournamentsPage.jsx
+++ client/src/components/pages/Tournaments/TournamentsPage.jsx
@@ -3,6 +3,11 @@ import { useNavigate } from "react-router-dom";
 import axiosInstance from "src/axiosInstance";
 import styles from "./TournamentsPage.module.css";
 import { Link } from "react-router-dom";
+import fonImage from "src/components/files/fon-main.jpg";
+import NavigationComp from "src/components/ui/Nav/NavigationComp";
+import BurgerMenuComp from "src/components/ui/Nav/BurgerMenuComp";
+import readyButtonImage from "src/components/pages/Tournaments/ready.png";
+
 
 export default function TournamentsPage({ user, logoutHandler }) {
   const [tournamentsList, setTournamentsList] = useState([]);
@@ -24,36 +29,60 @@ export default function TournamentsPage({ user, logoutHandler }) {
   };
 
   return (
-    <>
-      <h1>TournamentsPage</h1>
-      <div className={styles.twoCol}>
+    <div
+      className={styles.background}
+      style={{ backgroundImage: `url(${fonImage})` }}
+    >
+      <div className={styles.wrapper}>
+        <div className={styles.nav}>
+          <div className={styles.burger}>
+            <BurgerMenuComp />
+          </div>
+          <NavigationComp user={user} logoutHandler={logoutHandler} />
+        </div>
+
+        <h1>TournamentsPage</h1>
+        <div className={styles.twoCol}>
         <div className={styles.leftCol}>
-          {tournamentsList.map((tournament) => (
-            <div key={tournament.id} className={styles.tournamentCard}>
-              <div className={styles.date}>{tournament.date_start}</div>
-              <Link
-                to={`/tournaments/${tournament.id}`}
-                className={styles.tournamentLink}
-              >
-                <div className={styles.tournamentName}>{tournament.name}</div>
-              </Link>
-              <div className={styles.projectedCount}>
-                {tournament.projected_count_of_participants}
-              </div>
-              {tournament.isReady && (
-                <div className={styles.ready}>
-                  <button
-                    onClick={() => handleTournamentNavigation(tournament.id)}
-                  >
-                    Ready
-                  </button>
+          <h2>Сетка ФСМ</h2>
+          <div className={styles.elementListContainer}>
+            {tournamentsList.map((tournament) => (
+              <div key={tournament.id} className={styles.listElement}>
+                <div className={styles.date}>{tournament.date_start}</div>
+                <Link
+                  to={`/tournaments/${tournament.id}`}
+                  className={styles.tournamentLink}
+                >
+                  <div className={styles.tournamentName}>
+                    {tournament.name}
+                  </div>
+                </Link>
+                <div className={styles.num}>
+                  {tournament.projected_count_of_participants}
                 </div>
-              )}
-            </div>
-          ))}
+                {tournament.isReady && (
+                  <div className={styles.ready}>
+                    <button
+                    className={styles.readyButton}
+                      onClick={() => handleTournamentNavigation(tournament.id)}
+                      style={{backgroundImage: `url(${readyButtonImage})`}}
+                    >
+                      Ready
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+
+
         </div>
         <div className={styles.rightCol}></div>
       </div>
-    </>
+      </div>
+
+      
+      
+    </div>
   );
 }
