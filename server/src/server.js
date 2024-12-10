80fea453ec3c3963c68f1d25ffda5dcff939e6e6
milsimural <108474929+milsimural@users.noreply.github.com>
2024-12-10 15:28:33 +0500

    Апдейт сервака

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+const port = process.env.APP_PORT || 3000;
+if (process.env.APP_IP && process.env.APP_PATH) {
+  const ip = process.env.APP_IP;
+  const path = process.env.APP_PATH;
+  app.listen(port, ip, () => {
+    console.log(`Server listening on port ${port} and ip ${ip}`);
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+}
